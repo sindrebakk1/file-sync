b9dc43bde1a6b47b0ed696660e05a659e06dd9a6
Sindre Bakken <sindre.bakken.naesset@gmail.com>
2024-05-09 15:42:30 +0200

    add FileInfo struct type

--- pkg/models/file.go
+++ pkg/models/file.go
@@ -32,10 +32,34 @@ func (f *FileInfoBytes) GetTimestamp() time.Time {
 	return time.Unix(int64(binary.BigEndian.Uint16(f[FileHashSize+ChecksumSize:FileHashSize+ChecksumSize+TimestampSize])), 0)
 }
 
+func (f *FileInfoBytes) GetFileInfo() *FileInfo {
+	return &FileInfo{
+		Hash:      f.GetHash(),
+		Checksum:  f.GetChecksum(),
+		Timestamp: f.GetTimestamp(),
+	}
+}
+
 func NewFileInfoBytes(hash string, checksum string, timestamp time.Time) *FileInfoBytes {
 	var fileInfoBytes FileInfoBytes
 	copy(fileInfoBytes[:FileHashSize], hash)
 	copy(fileInfoBytes[FileHashSize:FileHashSize+ChecksumSize], checksum)
 	binary.BigEndian.PutUint16(fileInfoBytes[FileHashSize+ChecksumSize:FileInfoSize], uint16(timestamp.Unix()))
 	return &fileInfoBytes
+}
+
+type FileInfo struct {
+	Hash      string
+	Checksum  string
+	Timestamp time.Time
+}
+
+func (f *FileInfo) ToBytes() *FileInfoBytes {
+	return NewFileInfoBytes(f.Hash, f.Checksum, f.Timestamp)
+}
+
+func (f *FileInfo) FromBytes(fileInfoBytes *FileInfoBytes) {
+	f.Hash = fileInfoBytes.GetHash()
+	f.Checksum = fileInfoBytes.GetChecksum()
+	f.Timestamp = fileInfoBytes.GetTimestamp()
 }
